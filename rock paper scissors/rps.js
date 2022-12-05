7abb2971bb25e90bbe945657c530a2f0d24882b9
mrgrDennis <88109127+mrgrDennis@users.noreply.github.com>
2022-12-05 20:25:33 +0300

    rock paper scissors edit

--- rock paper scissors/rps.js
+++ rock paper scissors/rps.js
@@ -1,60 +1,45 @@
-
-let score1 = 0
-let score2 = 0
-
-console.log('enter your choice')
-
-ch = prompt('rock, paper or scissors... make a choice')
-let choices = ch.toLowerCase()
-
-let comp_choice = " "
-
-let value = Math.floor(Math.random() * 3)
-
-function computer() {
-  if (value == 0) {
-    comp_choice = 'rock'
-  }
-  else if (value == 1){
-    comp_choice = 'paper'
-  }
-  else if (value == 2){
-    comp_choice = 'scissors'
-  }
-  console.log("computer plays:"+comp_choice +' ' + "player plays:"+choices);
+function getComputerChoice(){
+    let value = Math.floor(Math.random() * 3)
+    if (value == 0) {
+        comp_choice = 'rock'
+      }
+      else if (value == 1){
+        comp_choice = 'paper'
+      }
+      else if (value == 2){
+        comp_choice = 'scissors'
+      }
+    return comp_choice
 }
-function pick() {
-  computer()
-  if (comp_choice == choices) {
-    console.log('draw')
-  }
-  else if (comp_choice == 'paper' && choices == 'rock') {
-    console.log('computer win');
-  }
-  else if (comp_choice == 'rock' && choices == 'scissors') {
-    console.log('computer win');
-  }
-  else if (comp_choice == 'scissors' && choices == 'paper') {
-    console.log('computer win');
-  }
-  else if (choices == 'paper' && comp_choice == 'rock') {
-    console.log('player win');
-  }
-  else if (choices == 'rock' && comp_choice == 'scissors') {
-    console.log('player win');
-  }
-  else if (choices == 'scissors' && comp_choice == 'paper') {
-    console.log('player win');
-  }
-  else {
-    console.log('error')
-  }
+
+let computerSelection = getComputerChoice()
+
+function playround(){
+
+    let select = prompt('rock, paper or scissors... make a choice')
+    let playerSelection = select.toLowerCase()
+
+    if (computerSelection === playerSelection) {
+        return 'draw'
+      }
+    else if (computerSelection == 'paper' && playerSelection == 'rock' || computerSelection == 'rock' && playerSelection == 'scissors' || computerSelection == 'scissors' && playerSelection == 'paper') {
+    return'computer win'
+    }
+
+    else if (playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'scissors' && computerSelection == 'paper') {
+    return 'player win'
+    }
+
+    else {
+    return 'error 001 : you didnt play by the rules!'
+    }   
 }
-for (var i = 0; i < 3; i++) {
-  choices = prompt('rock, paper or scissors... make a choice').toLowerCase()
-  comp_choice = ""
-  value = Math.floor(Math.random() * 3)
-  computer()
-  pick()
+
+function game(){
+    let score = 0
+    for (let i = 0; i < 5; i++){
+        playround()
+        console.log(playround())
+    }
 }
-pick()
+game()
